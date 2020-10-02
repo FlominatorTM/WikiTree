e714a33db97c4e3ff773617383756dffe5f2a8bd
Florian Straub <flominator@gmx.net>
2020-10-02 22:00:30 +0200

    update after UI changes at WikiTree

--- Greasemonkey_Shortcuts.js
+++ Greasemonkey_Shortcuts.js
@@ -1,21 +1,10 @@
 // ==UserScript==
 // @name          WikiTree input
 // @namespace     https://www.wikitree.com/
-// @description   adding some shortcuts and changing some links (see below for details)
+// @description   adding some shortcuts and changing some links
 // @match https://www.wikitree.com/*
 // @include   https://www.wikitree.com/
 // ==/UserScript==
-
-/*
-Features:
-
-- keyboard shortcuts for edit (E) and preview (P), see browser manual for details (in Firefox is ALT + SHIFT + shortcut)
-- removing redundant shortcut for save in order to make it work (this is a bug in the WikiTree UI)
-- showing private profile after saving instead of leaving edit mask open
-- automatically select "no middle name" if field is empty when editing a profile
-- show message box when there is no category present
-- replace German country names by English ones
-*/
 
 //shortcut e for edit button
 for (var i=0; ulNode = document.getElementsByTagName("ul")[i]; i++)
@@ -35,16 +24,6 @@ for (var i=0; ulNode = document.getElementsByTagName("ul")[i]; i++)
 }
 
 
-if((window.location + "").indexOf("?") == -1 && ""+document.title.indexOf("Edit") != -1)
-{
-  //remove "Edit Profile of" from tab title after saving
-	//document.title = document.getElementsByTagName("h1")[0].innerText.replace("Edit Profile of", "");
-  
-  //go to private view after saving
-  window.location=window.location+"?public=1";
-}
-
-
 //shortcut p for preview button
 var previewButton = document.getElementsByName("preview")[0];
 if(null != previewButton)
@@ -59,7 +38,14 @@ if(null != wpSaves)
   wpSaves[0].accessKey = "";
 }
 
-// automatically check "no middle name"
+//show profile after saving
+if(window.location == "https://www.wikitree.com/wiki/Special:EditPerson")
+{
+  var personID = document.getElementsByClassName("pureCssMenui0")[1].firstChild.innerHTML;
+  window.location="https://www.wikitree.com/wiki/" + personID;
+}
+
+//make middle name empty by default
 var inputMiddleName = document.getElementById('mMiddleName');
 if(null != inputMiddleName)
 {
@@ -71,26 +57,25 @@ if(null != inputMiddleName)
 
 insert_link_before("span", "previewbox", '<a name="preview_box">');
 
-//show message box when there is no "Category" present
 var wpTextbox = document.getElementById('wpTextbox1');
 if(wpTextbox != null && wpTextbox.value.indexOf("Category") == -1)
 {
 	alert("Category is missing");
 }
 
-//replace German country names by English ones
+//replace German texts
 var birthLocation = document.getElementById('mBirthLocation');
 if(birthLocation != null)
 {
-  birthLocation.value = birthLocation.value.replace("Deutschland", "Germany").replace("Vereinigte Staaten", "United States");
+  birthLocation.value = birthLocation.value.replace("Deutschland", "Germany").replace("Vereinigte Staaten", "United States of America");
 }
 
-//replace German country names by English ones
 var deathLocation = document.getElementById('mDeathLocation');
 if(deathLocation != null)
 {
-  deathLocation.value = deathLocation.value.replace("Deutschland", "Germany").replace("Vereinigte Staaten", "United States");
+  deathLocation.value = deathLocation.value.replace("Deutschland", "Germany").replace("Vereinigte Staaten", "United States of America");
 }
+
 
 //Insert any tag before any element with an id
 function insert_link_before(tagname, previous_element, linkcode)
