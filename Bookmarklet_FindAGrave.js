85956ce6d4fe6648f0cf64fcebdfce0fc3767ce5
Flominatortm <flominator@gmx.net>
2021-03-14 11:20:55 +0100

    removing duplicate files

--- Bookmarklet_FindAGrave.js
+++
@@ -1,9 +0,0 @@
-javascript: 
-var url = window.location+"";
-var needle = "memorial/";
-var indexOfMemorial = url.indexOf(needle) + needle.length;
-var indexOfSlashAfter = url.indexOf("/", indexOfMemorial);
-var memorialId = url.substring(indexOfMemorial, indexOfSlashAfter);
-
-prompt("..", "{{FindAGrave|" + memorialId +"}}");
-;void(0); 
