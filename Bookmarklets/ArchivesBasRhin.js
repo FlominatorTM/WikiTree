fc9a2683d26e80c823516a9051b17188d8d6356c
Florian Straub <flominator@gmx.net>
2021-03-17 19:56:08 +0100

    adding archive name
    via https://www.wikitree.com/g2g/1203548/automatically-create-source-citations-from-archives-rhin?show=1205447#c1205447

--- Bookmarklets/ArchivesBasRhin.js
+++ Bookmarklets/ArchivesBasRhin.js
@@ -1,8 +1,9 @@
 javascript:
 /* usage: select headline and enter page numbers */
+var archive = document.getElementsByName('description')[0].getAttribute('content');
 var book = window.getSelection()+'';
 var link = window.location + "";
 var pageCurrent = prompt("current page");
 var pageTotal = prompt("total pages");
-prompt("", book.trim() + ", Image " + pageCurrent + "/" + pageTotal + ": "+ link);
+prompt("", archive + ", " + book.trim() + ", Image " + pageCurrent + "/" + pageTotal + ": "+ link);
 ;void(0);
