00a1216fad4f75566bff1b84dd1b40699509da3e
Flominatortm <flominator@gmx.net>
2021-05-26 21:47:51 +0200

    only run when on actual source page

--- Greasemonkey_FamilySearch.js
+++ Greasemonkey_FamilySearch.js
@@ -14,41 +14,44 @@ function addFilmInfo()
   var filmNumber;
   var pageNumber;
   
-  for(var i=0; aNode = document.getElementsByTagName("a")[i]; i++)
+  if((window.location +"").indexOf("ark") > 0)
   {
-      var indexOfFilmNr = aNode.href.indexOf(strFilmNumberPrefix);
-      if(indexOfFilmNr > 0)
-      {
-          filmNumber = aNode.innerHTML;
-          var tr = aNode.parentNode.parentNode.parentNode;
-          var tdWithPageNumber= tr.nextSibling.childNodes[1];
-          pageNumber = tdWithPageNumber.firstChild.innerHTML;
-          break;
-        
-        /* 	above code navigates through this DOM tree:
-        
-						<a href="https://www.familysearch.org/search/record/results?q.filmNumber=008114118" class="css-1ejm1ub-linkCss">008114118</a>
-					</span>
-         </td>
-        </tr><!-- this is the tr after calling parentNode 3 times -->
-						<tr class="css-17zuftn"><!-- this is the next sibling -->
-            	<th scope="row" class="css-18jjc9d"><span secondary="" class="css-wikv0x"><strong>Aufnahmenummer</strong></span></th><!-- child 0 -->
-                <td class="css-18jjc9d"><!-- child 1: tdWithPageNumber-->
-                	<span secondary="" class="css-wikv0x"><!-- innerHTML of first child-->00290</span>        */
-      }
-  }
+    for(var i=0; aNode = document.getElementsByTagName("a")[i]; i++)
+    {
+        var indexOfFilmNr = aNode.href.indexOf(strFilmNumberPrefix);
+        if(indexOfFilmNr > 0)
+        {
+            filmNumber = aNode.innerHTML;
+            var tr = aNode.parentNode.parentNode.parentNode;
+            var tdWithPageNumber= tr.nextSibling.childNodes[1];
+            pageNumber = tdWithPageNumber.firstChild.innerHTML;
+            break;
 
-  var pageNumberInt = parseInt(pageNumber, 10);
-  var filmInfo = "FHL film " + parseInt(filmNumber, 10);
-  if(!isNaN(pageNumberInt)) //not all sources have image numbers
-  {
-    filmInfo = filmInfo + ", image " + pageNumberInt;
-  }
+          /* 	above code navigates through this DOM tree:
 
-  
-  /* "Deutschland, Rheinland-Pfalz, Diözese Mainz, Katholische Kirchenbücher, 1540-1952", database, FamilySearch (https://www.familysearch.org/ark:/61903/1:1:DTBJ-FL3Z : 8 December 2020), Georg Fertig, 1831. */
-  // now let's insert the filmInfo before the name
-  var citation = document.getElementById('citation').innerHTML.replace("),", "), " + filmInfo +",");
-  //and replace the existing citation by the new one
-  document.getElementById('citation').innerHTML = citation;
+              <a href="https://www.familysearch.org/search/record/results?q.filmNumber=008114118" class="css-1ejm1ub-linkCss">008114118</a>
+            </span>
+           </td>
+          </tr><!-- this is the tr after calling parentNode 3 times -->
+              <tr class="css-17zuftn"><!-- this is the next sibling -->
+                <th scope="row" class="css-18jjc9d"><span secondary="" class="css-wikv0x"><strong>Aufnahmenummer</strong></span></th><!-- child 0 -->
+                  <td class="css-18jjc9d"><!-- child 1: tdWithPageNumber-->
+                    <span secondary="" class="css-wikv0x"><!-- innerHTML of first child-->00290</span>        */
+        }
+    }
+
+    var pageNumberInt = parseInt(pageNumber, 10);
+    var filmInfo = "FHL film " + parseInt(filmNumber, 10);
+    if(!isNaN(pageNumberInt)) //not all sources have image numbers
+    {
+      filmInfo = filmInfo + ", image " + pageNumberInt;
+    }
+
+
+    /* "Deutschland, Rheinland-Pfalz, Diözese Mainz, Katholische Kirchenbücher, 1540-1952", database, FamilySearch (https://www.familysearch.org/ark:/61903/1:1:DTBJ-FL3Z : 8 December 2020), Georg Fertig, 1831. */
+    // now let's insert the filmInfo before the name
+    var citation = document.getElementById('citation').innerHTML.replace("),", "), " + filmInfo +",");
+    //and replace the existing citation by the new one
+    document.getElementById('citation').innerHTML = citation;
+	}
 }
