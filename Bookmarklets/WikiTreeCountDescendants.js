402abcaa22d1ef7fdddeedf913d83989d8da9b15
Florian Straub <flominator@gmx.net>
2021-06-08 16:19:14 +0200

    renaming variables

--- Bookmarklets/WikiTreeCountDescendants.js
+++ Bookmarklets/WikiTreeCountDescendants.js
@@ -1,10 +1,10 @@
 javascript:
 /* counts the number of decendants (via https://www.wikitree.com/g2g/1249590/simple-idea-for-the-descendants-list-within-profiles?show=1250209 ) */
-for (var i=0; aNode = document.getElementsByTagName("ol")[i]; i++)
+for (var i=0; olNode = document.getElementsByTagName("ol")[i]; i++)
 {
-    var OLs = aNode.getElementsByTagName("li").length;
-    var ins_li = document.createElement("span");
-            ins_li.innerHTML="Descendants: " + OLs;
-             aNode.parentNode.insertBefore(ins_li, aNode);
+    var liCount = olNode.getElementsByTagName("li").length;
+    var spanNode = document.createElement("span");
+    spanNode.innerHTML="Descendants: " + liCount;
+    olNode.parentNode.insertBefore(spanNode, olNode);
  }        
 void(0);
