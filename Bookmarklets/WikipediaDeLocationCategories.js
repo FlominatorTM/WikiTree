05bb2311d51fb64f25dff6586bd12a111a6866a4
Florian Straub <flominator@gmx.net>
2022-08-03 20:16:26 +0200

    moved comment on top and removed duplicate pipe

--- Bookmarklets/WikipediaDeLocationCategories.js
+++ Bookmarklets/WikipediaDeLocationCategories.js
@@ -1,8 +1,8 @@
-/* creates template for a category when being on entry of German Wikipedia article about the place */
 javascript:  
+/* creates template for a category when being on entry of German Wikipedia article about the place */
 var lat = document.getElementsByClassName("latitude")[0].innerHTML + ""; 
 var lon = document.getElementsByClassName("longitude")[0].innerHTML + ""; 
-var coor = "|coordinate=" + lat + "," + lon;  
+var coor = "coordinate=" + lat + "," + lon;  
 var wikidata = "|wikidataID=";  
 var allAnkerNodes = document.getElementsByTagName("a"); 
 for (var i=0; i < allAnkerNodes.length ; i++)  
