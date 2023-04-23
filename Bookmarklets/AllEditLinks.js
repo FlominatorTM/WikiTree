7e196153263e39c2450b2ceb6afe04106595cbe7
Flominatortm <flominator@gmx.net>
2023-04-23 10:07:43 +0200

    renaming to .js

--- Bookmarklets/AllEditLinks.js
+++ Bookmarklets/AllEditLinks.js
@@ -1,17 +1,17 @@
 javascript: 
-/* 
- this will temporarily change all links to wiki pages to directly open in edit view
- one can also add customer parameters like wpSummary (which will fill the summary field
- in combination with Greasemonkey_Shortcuts.js
-*/
-var strNewHref = prompt('enter URL part that links should be replaced by (w= for IDs like Straub-620, u= is for numbers', '/index.php?title=Special:EditPerson&wpSummary=&w=');
 var allAnkerNodes = document.getElementsByTagName("a");
 for (var i=0; i < allAnkerNodes.length ; i++) 
 {     
-	if ( href = allAnkerNodes[i].getAttribute("href") )     
+	if (href = allAnkerNodes[i].getAttribute("href"))
 	{ 
-		href = href.replace(/\/wiki\//, strNewHref);
-		allAnkerNodes[i].setAttribute("href", href);  
+		hrefText = (""+ href);
+		if(hrefText.search("/@")>-1)
+		{
+			hrefText = hrefText.replace("toot.community/@", "genealysis.social/@");
+		allAnkerNodes[i].href = hrefText /*.replace("/@", "genealysis.social/@")*/;
+	alert(allAnkerNodes[i].href);
+		}
+
 	} 
 } 
 void(0);
