a1fe6c4fe47e9a1d9c19931e2ad7a1782af088aa
Flominatortm <flominator@gmx.net>
2023-05-06 05:29:48 +0200

    feature switches

--- ProjectMembers.py
+++ ProjectMembers.py
@@ -1,8 +1,18 @@
 import requests
 import dateutil.parser
+import argparse
 
 from datetime import datetime 
 from dateutil.relativedelta import relativedelta 
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Creates a report of one project')
+    parser.add_argument('--contribs', action='store_true', help='Checks edited profiles for keyword')
+    parser.add_argument('--last', action='store_true', help='Gets last edit')
+    parser.add_argument('--any', action='store_true', help='Checks for edits in last months')
+    parser.add_argument('--checkin', action='store_true', help='Checks is user received check-in message')
+    return parser.parse_args()
+
 
 def get_member_users(project):
 
@@ -43,6 +53,8 @@ def get_member_users(project):
     return members
 
 def check_edit_history(theUser):
+    global args
+    
     link = "https://www.wikitree.com/index.php?title=Special:Contributions&l=500&who=" + theUser["id"];
     f = requests.get(link)
     contribPage = f.text
@@ -71,14 +83,17 @@ def check_edit_history(theUser):
             theUser["lastEdit"] = editDate
             theUser["editedProject"] = False
             theUser["anyEdit"] = False
+            if args.last is False and args.contribs is False:
+                break
         
         print (editDateFormatted)
         if editDate > six_months_ago:
             theUser["anyEdit"] = True
-            relevant_edit = did_user_perform_relevant_edit(oneDay)
-            if relevant_edit is True:
-                theUser["editedProject"] = True
-                break
+            if args.contribs is True:
+                relevant_edit = did_user_perform_relevant_edit(oneDay)
+                if relevant_edit is True:
+                    theUser["editedProject"] = True
+                    break
             any_edit = True
         else:
             print("edit too old, done")
@@ -124,21 +139,32 @@ def does_profile_contain(link, words):
         
 
 def get_checkin_requested(theUser, checkInSubstring):
+    if args.checkin is False:
+        return
     link = 'https://www.wikitree.com/wiki/' + theUser["id"]
     f = requests.get(link)
     userPage = f.text
     theUser["check-in-requested"] = checkInSubstring in userPage
 
 def write_report(members):
+    global args
     f = open("members.htm", "w")
     f.write('<html><head></head><body><table border="1">')
     f.write("<tr>")
     f.write("<th>ID</th>")
     f.write("<th>Name</th>")
-    f.write("<th>Last edit</th>")
-    f.write("<th>Check in?</th>")
-    f.write("<th>Any edit?</th>")
-    f.write("<th>Project edit?</th>")
+    
+    if args.last is True:
+        f.write("<th>Last edit</th>")
+        
+    if args.checkin is True:
+        f.write("<th>Check in?</th>")
+    
+    if args.any is True:
+        f.write("<th>Any edit?</th>")
+    
+    if args.contribs is True:
+        f.write("<th>Project edit?</th>")
     f.write("<th>Badge</th>")
     f.write("</tr>")
     for member in sorted(members, key=lambda d: d['lastEdit']):
@@ -149,27 +175,35 @@ def write_report(members):
         f.write("<td>")
         f.write(member["name"])
         f.write("</td>")
-        f.write("<td>")
-        f.write( member["lastEditFormatted"])
-        f.write("</td>")
-        f.write("<td>")
-        if member["check-in-requested"]:
-            f.write("yes")
-        else:
-            f.write("no")
-        f.write("</td>")
-        f.write("<td>")
-        if member["anyEdit"]:
-            f.write("yes")
-        else:
-            f.write("no")
-        f.write("</td>")    
-        f.write("<td>")
-        if member["editedProject"]:
-            f.write("yes")
-        else:
-            f.write("no")
-        f.write("</td>")
+
+        if args.last is True:
+            f.write("<td>")
+            f.write( member["lastEditFormatted"])
+            f.write("</td>")
+            
+        if args.checkin is True:
+            f.write("<td>")
+            if member["check-in-requested"]:
+                f.write("yes")
+            else:
+                f.write("no")
+            f.write("</td>")
+
+        if args.any is True:
+            f.write("<td>")
+            if member["anyEdit"]:
+                f.write("yes")
+            else:
+                f.write("no")
+            f.write("</td>")    
+        
+        if args.contribs is True:
+            f.write("<td>")
+            if member["editedProject"]:
+                f.write("yes")
+            else:
+                f.write("no")
+            f.write("</td>")
         f.write("<td>")
         f.write('<a href="https://www.wikitree.com/index.php?title=Special:AwardBadge&badge_id=180&users='+ member["id"]+'&action=remove">remove now</a>')
         f.write("</td>")
@@ -178,9 +212,7 @@ def write_report(members):
     f.close()
         
 
-parser = argparse.ArgumentParser(description='Creates a report of one project')
-parser.add_argument('--contribs', action='store_true', help='Checks edited profiles for keyword')
-args = parser.parse_args()
+args = get_args()
 
 
 profiles_global = {}
@@ -199,3 +231,4 @@ for member in members:
     get_checkin_requested(member, "It’s Germany Project check-in time again")
     print(member)
 
+write_report(members)
