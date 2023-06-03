36c31a20e79cd44f1e45b45ae26b75c983194252
Flominatortm <flominator@gmx.net>
2023-06-03 20:19:46 +0200

    +PortaFontium.js

---
+++ Bookmarklets/PortaFontium.js
@@ -0,0 +1,14 @@
+javascript:
+ 
+var citation = "Porta fontium";
+var strongPlaceName = document.getElementsByClassName("iipimage")[0];
+var place = strongPlaceName.innerText;
+var durationAndKindSpan = strongPlaceName.parentNode.nextSibling.nextSibling.nextSibling.nextSibling;
+var durationAndKind = durationAndKindSpan.firstChild.nextSibling.nextSibling.nextSibling.innerHTML;
+var lastImage = hdv.options.imagesSet.length;
+var currentImage = hdv.osdViewer.currentPage()+1;
+
+citation += " > " + place + " > " + durationAndKind + " > [" + window.location.href.replace(location.search, '') +" picture " + currentImage + "/" + lastImage + "]";
+alert(citation);
+
+void(0);
