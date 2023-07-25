279dfd7bc14978a86d7277b6e915d871f95a4560
Florian Straub <flominator@gmx.net>
2023-07-25 08:46:01 +0200

    Update WikipediaDeLocationCategoriesTNG.js
    supporting Verbandsgemeinde

--- Bookmarklets/WikipediaDeLocationCategoriesTNG.js
+++ Bookmarklets/WikipediaDeLocationCategoriesTNG.js
@@ -18,6 +18,7 @@ for (var i=0; i < states.length ; i++)
 
 theState = states[statePosition];
 theCounty = "";
+hasVerbandsgemeinde = window.document.body.innerHTML.indexOf("Verbandsgemeinde") > -1;
 for (var i=0; aTag = document.getElementsByTagName("a")[i]; i++)
 {
 	if(aTag.innerText.toLowerCase().indexOf("kreis") >- 1 && aTag.innerText != "Landkreis")
@@ -27,11 +28,21 @@ for (var i=0; aTag = document.getElementsByTagName("a")[i]; i++)
 		{
 			theCounty = theCounty + " (Kreis)";
 		}
-		break;
+        if(!hasVerbandsgemeinde)
+        {
+            /* might come later */
+            break;
+        }
 	}
+    else if(aTag.innerText.startsWith("Verbandsgemeinde")&& aTag.innerText != "Verbandsgemeinde")
+    {
+        theCounty = aTag.innerText;
+        break;
+    }
+
 }
 
-
+theCounty = theCounty.trim();
 var parentPlace = window.getSelection()+ "";
 
 if(parentPlace != "")
@@ -76,6 +87,5 @@ else
 }
 
 var article = document.getElementsByClassName('mw-page-title-main')[0].innerText;
-
 var new_window = window.open("https://www.wikitree.com/index.php?title=Category:" + article  + ", " + theState+ "&action=edit");
 void(0);
