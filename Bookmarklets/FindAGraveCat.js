bc6e78d22ad2ab762417c91c28fb03cc685d693a
Flominatortm <flominator@gmx.net>
2023-08-11 21:06:30 +0200

    FindAGraveCat.js: cemeteries without coordinates

--- Bookmarklets/FindAGraveCat.js
+++ Bookmarklets/FindAGraveCat.js
@@ -1,52 +1,76 @@
-
 javascript:
-  var _id = document.getElementsByClassName('copyme')[0].innerText;
-  var _name = document.querySelector('[itemprop=name]').innerText;
-  
-  var _lat = document.querySelector('[title=Latitude\\:]').innerText;
-  var _lon = document.querySelector('[title=Longitude\\:]').innerText;
-  var _location = document.querySelector('[itemprop=addressLocality]').innerText;
-  var _locationParts = _location.split(", ");
-  var _county = _locationParts[_locationParts.length-1];
+var IS_BOOKMARKLET = true;
+var _id = document.getElementsByClassName('copyme')[0].innerText;
+var _name = document.querySelector('[itemprop=name]').innerText;
 
-  var _state =  document.querySelector('[itemprop=addressRegion]').innerText;
-  var _country = document.querySelector('[itemprop=addressCountry]').innerText;
-  var _catName = _name + ", " + _locationParts[0] + ", " + _state;
-  var _address = "";
-  try {
-      _address = document.querySelectorAll('[itemprop=address]')[1].innerHTML.replace("<br>", ", ").trim();
-  }
-  catch {
-  }
+var _lat = "";
+var _lon = "";
 
-  var _aka = "";
-  try {
-      _aka = document.querySelector('[itemprop=alternateName]').innerText;
-  }
-  catch {
-    
-  }
+try
+{
+_lat = document.querySelector('[title=Latitude\\:]').innerText;
+_lon = document.querySelector('[title=Longitude\\:]').innerText;
+}
+catch
+{}
 
-  var output = "{{CategoryInfoBox Cemetery " +
-    "\n|name=" + _name +
-    "\n|aka=" + _aka  +
-    "\n|address=" + _address +
-    "\n|parent=" + _county + ", " + _state +
-    "\n|location=" + _county + ", " + _state +
-    "\n|spacepage= "+
-    "\n|wikidataID="+
-    "\n|findagraveID=" + _id +
-    "\n|billiongravesID= "+
-    "\n|webpage= "+
-    "\n|searchwebpage= "+
-    "\n|coordinate="+ _lat + "," + _lon +
-    "\n|startdate= "+
-    "\n|enddate=\n}}";
+var _location = document.querySelector('[itemprop=addressLocality]').innerText;
+var _locationParts = _location.split(", ");
+var _county = _locationParts[_locationParts.length-1];
+var _state =  document.querySelector('[itemprop=addressRegion]').innerText;
+var _locationCategory = _county + ", " + _state;
+if(!IS_BOOKMARKLET && _locationParts.length > 1)
+{
+const potentialCityCategory = _locationParts[0] + ", " + _state;
+let catUrl = "https://www.wikitree.com/wiki/Category:" + encodeURI(potentialCityCategory);
+let xmlHttp = new XMLHttpRequest();
+xmlHttp.open("GET", catUrl, false); /* false for synchronous request */
+xmlHttp.send(null);
+if (xmlHttp.status < 400) {
+  _locationCategory = potentialCityCategory;
+} 
+}
 
-  var billon = "https://www.google.com/search?q=" + encodeURIComponent("site:https://billiongraves.com " + _name + " " + _location);
-  var win = window.open(billon);
-  EditCategoryDiff(output, _catName);
-void(0);
+/*var _country = document.querySelector('[itemprop=addressCountry]').innerText;*/
+var _catName = _name + ", " + _locationParts[0] + ", " + _state;
+var _address = "";
+try 
+{
+  _address = document.querySelectorAll('[itemprop=address]')[1].innerHTML.replace("<br>", ", ").trim();
+}
+catch 
+{
+}
+
+var _aka = "";
+try 
+{
+  _aka = document.querySelector('[itemprop=alternateName]').innerText;
+}
+catch 
+{
+
+}
+
+var output = "{{CategoryInfoBox Cemetery " +
+"\n|name=" + _name +
+"\n|aka=" + _aka  +
+"\n|address=" + _address +
+"\n|parent=" + _county + ", " + _state +
+"\n|location=" + _locationCategory +
+"\n|spacepage= "+
+"\n|wikidataID="+
+"\n|findagraveID=" + _id +
+"\n|billiongravesID= "+
+"\n|webpage= "+
+"\n|searchwebpage= "+
+"\n|coordinate="+ _lat + "," + _lon +
+"\n|startdate= "+
+"\n|enddate=\n}}";
+
+var billon = "https://www.google.com/search?q=" + encodeURIComponent("site:https://billiongraves.com " + _name + " " + _location);
+var win = window.open(billon);
+EditCategoryDiff(output, _catName);
 
 function EditCategoryDiff(output, catName)
 {
