2e909616437e3a853204db59b91cdeb88fed51f1
Flominatortm <flominator@gmx.net>
2023-11-02 20:59:59 +0100

    Archion.js: cleanup

--- Bookmarklets/Archion.js
+++ Bookmarklets/Archion.js
@@ -1,33 +1,30 @@
-javascript:
- 
-var citation = "Archion";
-var breadCrumbList =[0];
-for (var i=1; valNode = document.getElementsByClassName('breadcrumb-item mb-0')[i]; i++)
-{
-	
-		citation += " > " + valNode.innerText;
-	
+javascript: var citation = "Archion";
+
+for (
+  var i = 1;
+  (valNode = document.getElementsByClassName("breadcrumb-item mb-0")[i]);
+  i++
+) {
+  citation += " > " + valNode.innerText;
 }
 
 var permaLinkDivs = document.getElementsByClassName("inner");
 var permaLink;
-if(null != permaLinkDivs && permaLinkDivs.length > 0)
-{
-	permaLinkDiv = permaLinkDivs[0];
-	permaLink = permaLinkDiv.getElementsByTagName("a")[0].innerText;
-}
-else
-{
-	permaLink = prompt("Please enter permalink or enable permalink and try again");
+if (null != permaLinkDivs && permaLinkDivs.length > 0) {
+  permaLinkDiv = permaLinkDivs[0];
+  permaLink = permaLinkDiv.getElementsByTagName("a")[0].innerText;
+} else {
+  permaLink = prompt(
+    "Please enter permalink or enable permalink and try again"
+  );
 }
 
-if (permaLink != null)
-{
-	var select_id = document.getElementsByClassName("page-select")[0];
-	var imageName = select_id.options[select_id.selectedIndex].text;
+if (permaLink != null) {
+  var select_id = document.getElementsByClassName("page-select")[0];
+  var imageName = select_id.options[select_id.selectedIndex].text;
 
-	citation += " > picture [" + permaLink + " " + imageName +"], #";
+  citation += " > picture [" + permaLink + " " + imageName + "], #";
 
-	alert(citation);
+  alert(citation);
 }
-void(0);
+void 0;
