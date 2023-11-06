c4c7211a302c05bc0cdecbc3ce139697a78ed301
Flominatortm <flominator@gmx.net>
2023-11-06 12:40:17 +0100

    g2gpeek; recognizing comments as well

--- AppsServer/g2gpeek.php
+++ AppsServer/g2gpeek.php
@@ -3,33 +3,50 @@
 $post = $_REQUEST['post'];
 $answer = $_REQUEST['a'];
 
-
-$url = "$post?show=$answer#a$answer";
+$url = "$post?show=$answer&appId=Straub620_g2gpeek#a$answer";
 
 echo $url;
 $page = file_get_contents($url);
+// echo "get done<br>";
 
 $doc = new DOMDocument();
 @$doc->loadHTML($page);
+// echo "load done<br>";
+
+// echo "a=$answer<br>";
 
 $aTags = $doc->getElementsByTagName("a");
 $len = $aTags->length;
-for($i=0;$i<$len;$i++)
-{
-    if($aTags[$i]->getAttribute("name") == $answer)
-    {
-        echo "hallo";
-        $qaaitemmeta = $aTags[$i]->parentNode->nextSibling->nextSibling->firstChild->nextSibling;
-        $qaaitemwho = $qaaitemmeta->childNodes[5];
-        $user = $qaaitemwho->childNodes[3]->nodeValue;
-        
+for ($i = 0; $i < $len; $i++) {
+    // echo "$i<hr><br>";
+    // echo $aTags[$i]->getAttribute("name") . "<br>";
+
+    if ($aTags[$i]->getAttribute("name") == $answer) {
+
+        $qaaitemavatarmeta = $aTags[$i]->parentNode->nextSibling->nextSibling->firstChild->nextSibling;
+        $qaaitemwho = $qaaitemavatarmeta->childNodes[5];
+
+        $user = "";
+        $what = "";
+        if ($qaaitemwho != null && $qaaitemwho->childNodes[3] != null && $qaaitemwho->childNodes[3]->nodeValue != null) {
+            $user = $qaaitemwho->childNodes[3]->nodeValue;
+            $what = "Answer";
+        } else {
+
+            $qacitemmeta = $qaaitemavatarmeta->childNodes[1];
+            $qaaitemwho = $qacitemmeta->childNodes[5];
+            $user = $qaaitemwho->childNodes[3]->nodeValue;
+            $what = "Comment";
+            $url = str_replace("#a", "#c", $url);
+        }
+
         header('Content-Type: text/html; charset=utf-8');
         echo "<html>";
         echo " <head>";
-        echo '<meta http-equiv="refresh" content="0; URL=' . $url . '">';
-        echo '  <meta property="og:description" content="' .  $aTags[$i]->nextSibling->textContent  .'">';
-        echo '  <meta property="og:title" content="Answer by ' . $user .  '">';
-        echo '  <title>Answer by ' . $user .' </title>';
+        echo '<meta http-equiv="refresh" content="0; URL=' . str_replace("&appId=Straub620_g2gpeek", "", $url) . '">';
+        echo '  <meta property="og:description" content="' .  $aTags[$i]->nextSibling->textContent  . '">';
+        echo '  <meta property="og:title" content="' . $what . ' by ' . $user .  '">';
+        echo '  <title>' . $what . ' by ' . $user . ' </title>';
         echo " </head>";
         echo " <body>";
         echo " </body>";
@@ -37,6 +54,3 @@ for($i=0;$i<$len;$i++)
         break;
     }
 }
-
-
-?>
