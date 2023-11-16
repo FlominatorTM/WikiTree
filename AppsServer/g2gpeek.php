f7a96e624ecdabb277e0160e0b25c3996c7e2bfe
Flominatortm <flominator@gmx.net>
2023-11-16 21:16:15 +0100

    g2gpeek.php rewrote WBE component for Discord preview links from G2G

--- AppsServer/g2gpeek.php
+++ AppsServer/g2gpeek.php
@@ -1,56 +1,44 @@
 <?php
-
 $post = $_REQUEST['post'];
 $answer = $_REQUEST['a'];
 
 $url = "$post?show=$answer&appId=Straub620_g2gpeek#a$answer";
 
-echo $url;
+echo "<!--$url--->";
+echo "please hold the line ...";
 $page = file_get_contents($url);
-// echo "get done<br>";
 
-$doc = new DOMDocument();
-@$doc->loadHTML($page);
-// echo "load done<br>";
+$tagsBeforeText = '<a name="' . $answer . '"></a><div itemprop="text">';
+$indexBeforeText = strpos($page, $tagsBeforeText) + strlen($tagsBeforeText);
 
-// echo "a=$answer<br>";
+$tagsAfterText = '<span class="qa';
+$indexAfterText = strpos($page, $tagsAfterText, $indexBeforeText);
+$lengthText = $indexAfterText - $indexBeforeText;
+$msg = substr($page, $indexBeforeText, $lengthText);
 
-$aTags = $doc->getElementsByTagName("a");
-$len = $aTags->length;
-for ($i = 0; $i < $len; $i++) {
-    // echo "$i<hr><br>";
-    // echo $aTags[$i]->getAttribute("name") . "<br>";
+$tagBeforeUserName = '="qa-user-link">';
+$indexStartUserName = strpos($page, $tagBeforeUserName, $indexAfterText);
+$indexStartUserName += strlen($tagBeforeUserName);
+$indexEndUserName = strpos($page, "</", $indexStartUserName); //spelling of </a> differs
+$lengthUserName = $indexEndUserName - $indexStartUserName;
+$user = substr($page, $indexStartUserName, $lengthUserName);
 
-    if ($aTags[$i]->getAttribute("name") == $answer) {
-
-        $qaaitemavatarmeta = $aTags[$i]->parentNode->nextSibling->nextSibling->firstChild->nextSibling;
-        $qaaitemwho = $qaaitemavatarmeta->childNodes[5];
-
-        $user = "";
-        $what = "";
-        if ($qaaitemwho != null && $qaaitemwho->childNodes[3] != null && $qaaitemwho->childNodes[3]->nodeValue != null) {
-            $user = $qaaitemwho->childNodes[3]->nodeValue;
-            $what = "Answer";
-        } else {
-
-            $qacitemmeta = $qaaitemavatarmeta->childNodes[1];
-            $qaaitemwho = $qacitemmeta->childNodes[5];
-            $user = $qaaitemwho->childNodes[3]->nodeValue;
-            $what = "Comment";
-            $url = str_replace("#a", "#c", $url);
-        }
-
-        header('Content-Type: text/html; charset=utf-8');
-        echo "<html>";
-        echo " <head>";
-        echo '<meta http-equiv="refresh" content="0; URL=' . str_replace("&appId=Straub620_g2gpeek", "", $url) . '">';
-        echo '  <meta property="og:description" content="' .  htmlentities(strip_tags($aTags[$i]->nextSibling->textContent))  . '">';
-        echo '  <meta property="og:title" content="' . $what . ' by ' . $user .  '">';
-        echo '  <title>' . $what . ' by ' . $user . ' </title>';
-        echo " </head>";
-        echo " <body>";
-        echo " </body>";
-        echo " </html>";
-        break;
-    }
+$lengthBetween = $indexStartUserName - $indexAfterText;
+$between = substr($page, $indexAfterText, $lengthBetween);
+$what = "Answer";
+if (stristr($between, "qa-c-item-who-data")) {
+    $what = "Comment";
+    $url = str_replace("#a", "#c", $url);
 }
+
+header('Content-Type: text/html; charset=utf-8');
+echo "<html>";
+echo " <head>";
+echo '<meta http-equiv="refresh" content="0; URL=' . str_replace("&appId=Straub620_g2gpeek", "", $url) . '">';
+echo '  <meta property="og:description" content="' . trim(htmlentities(strip_tags($msg)))  . '">';
+echo '  <meta property="og:title" content="' . $what . ' by ' . $user .  '">';
+echo '  <title>' . $what . ' by ' . $user . ' </title>';
+echo " </head>";
+echo " <body>";
+echo " </body>";
+echo " </html>";
