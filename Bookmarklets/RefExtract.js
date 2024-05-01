1d262fb6111d14fe0f37132ab02cc80906200fc5
Flominatortm <flominator@gmx.net>
2024-05-01 16:12:52 +0200

    RefExtract.js updates

--- Bookmarklets/RefExtract.js
+++ Bookmarklets/RefExtract.js
@@ -1,6 +1,7 @@
-javascript: var name = "name";
+javascript: var name = prompt("future name of this <ref>?");
+/* place cursor inside an inline reference before clicking */
 var wpTextbox = document.getElementById("wpTextbox1");
-var selectionStart = wptext.selectionStart;
+var selectionStart = wpTextbox.selectionStart;
 var indexEndRef = wpTextbox.value.indexOf("</ref>", selectionStart);
 var indexStartRef = wpTextbox.value.lastIndexOf("<ref>", indexEndRef);
 
@@ -22,8 +23,8 @@ if (indexEndRef == -1 || indexStartRef == -1) {
     refWithoutName,
     '<ref name="' + name + '"/>'
   );
-  wptext.selectionStart = selectionStart;
-  wptext.selectionEnd = selectionStart;
+  wpTextbox.selectionStart = selectionStart;
+  wpTextbox.selectionEnd = selectionStart;
   prompt("", refWithName);
 }
 void 0;
