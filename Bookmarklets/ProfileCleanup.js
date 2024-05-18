cc95e3160824023ec3f6fc7420a21bb9614a272d
Flominatortm <flominator@gmx.net>
2024-05-18 06:55:18 +0200

    ProfileCleanup: remove newlines before bio and empty bullets

--- Bookmarklets/ProfileCleanup.js
+++ Bookmarklets/ProfileCleanup.js
@@ -1,17 +1,29 @@
-javascript: var ta = document.getElementById("wpTextbox1");
+javascript: moveCategories();
+
+function moveCategories() {
+  var ta = document.getElementById("wpTextbox1");
   var parts = ta.value.split("\n");
   var oldValue = ta.value;
   var top_ = "";
   var bottom = "";
 
   for (var i = 0; i < parts.length; i++) {
+    if (parts[i].trim() == "*") {
+      continue;
+    }
     if (parts[i].indexOf("[[Category") > -1) {
       top_ += "\n" + parts[i].replace("* [[Category", "[[Category");
     } else {
       bottom += "\n" + parts[i];
     }
   }
+
   if (top_ != "") {
-    ta.value = top_.substring(1) + bottom;
+    bottom = bottom.replace(/^\n+/, "");
+    ta.value = top_.substring(1) + "\n" + bottom;
+    if (oldValue != ta.value) {
+      document.getElementById("wpSummary").value = "Categories up. ";
+    }
   }
-  void(0);
+}
+void 0;
