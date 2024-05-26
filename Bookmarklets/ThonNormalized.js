ba14c08d9d99eb865a7637a49d85b6d275d39bc9
Flominatortm <flominator@gmx.net>
2024-05-26 18:22:00 +0200

    ThonNormalized: show differences to previous and next position as well

--- Bookmarklets/ThonNormalized.js
+++ Bookmarklets/ThonNormalized.js
@@ -1,5 +1,6 @@
+javascript: var dummy;
 /* known issues: doesn't work with identical averages */
-javascript: var nameTDs = document.getElementsByClassName(
+var nameTDs = document.getElementsByClassName(
   "level1 groupC groupL groupR groupT"
 );
 var pointTDs = document.getElementsByClassName(
@@ -13,6 +14,7 @@ var points = [];
 var dict = {};
 var numMembers = [];
 var indexMembers = 0;
+
 for (var i = 0; (tdNode = nameTDs[i]); i++) {
   var pointsForThisTeam = parseFloat(
     pointTDs[i].innerText.replace(".", "").replace(",", "")
@@ -32,7 +34,7 @@ for (var i = 0; (tdNode = nameTDs[i]); i++) {
   normalizedPoints = Math.round(normalizedPoints * 100) / 100;
 
   while (normalizedPoints in dict) {
-    if (normalizedPoints.includes(".")) {
+    if (normalizedPoints.toString().includes(".")) {
       normalizedPoints += "0";
     } else {
       normalizedPoints += ".0";
@@ -40,7 +42,25 @@ for (var i = 0; (tdNode = nameTDs[i]); i++) {
   }
   dict[normalizedPoints] = tdNode.innerText;
   console.log(normalizedPoints + "=>" + tdNode.innerText);
+
+  if (i > 0) {
+    var pointsLastTeam = parseFloat(
+      pointTDs[i - 1].innerText.replace(".", "").replace(",", "")
+    );
+    var diffToHigher = pointsLastTeam - pointsForThisTeam;
+    tdNode.innerHTML += "<br>-" + diffToHigher;
+  }
+
+  if (i < nameTDs.length - 1) {
+    var pointsNextTeam = parseFloat(
+      pointTDs[i + 1].innerText.replace(".", "").replace(",", "")
+    );
+    var diffToLower = pointsForThisTeam - pointsNextTeam;
+    tdNode.innerHTML += "<br>+" + diffToLower;
+  }
+
   points[i] = normalizedPoints;
+  pointsLastTeam = pointsForThisTeam;
 }
 
 points.sort(function (a, b) {
@@ -51,7 +71,17 @@ var pos = 1;
 
 res = "";
 for (var i = 0; i < points.length; i++) {
-  res += pos + ".  " + dict[points[i]] + ": " + points[i] + "\n";
+  res +=
+    pos + ".  " + dict[points[i]] + ": " + Math.round(points[i] * 100) / 100;
+  if (i > 0) {
+    var diffToHigher = points[i - 1] - points[i];
+    res += " -" + Math.round(diffToHigher * 100) / 100 + "";
+  }
+  if (i < points.length - 1) {
+    var diffToLower = points[i] - points[i + 1];
+    res += " +" + Math.round(diffToLower * 100) / 100 + "";
+  }
+  res += "\n";
   pos++;
 }
 
