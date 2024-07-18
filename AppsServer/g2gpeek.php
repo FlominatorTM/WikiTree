9df9b2193204ded0a96cdf901cef6b5500ad981b
Flominatortm <flominator@gmx.net>
2024-07-18 20:35:53 +0200

    fix g2gpeek show substitute text if answer is not found

--- AppsServer/g2gpeek.php
+++ AppsServer/g2gpeek.php
@@ -1,7 +1,7 @@
 <?php
 $post = $_REQUEST['post'];
 $answer = $_REQUEST['a'];
-
+$is_debug = isset($_REQUEST['debug']);
 $url = "$post?show=$answer&appId=Straub620_g2gpeek#a$answer";
 
 echo "<!--$url--->";
@@ -9,43 +9,78 @@ echo "please hold the line ...";
 $page = file_get_contents($url);
 
 $tagsBeforeText = '<a name="' . $answer . '"></a><div itemprop="text">';
-$indexBeforeText = strpos($page, $tagsBeforeText) + strlen($tagsBeforeText);
+print_debug("tagsBeforeText=" . $tagsBeforeText);
 
-$tagsAfterText = '<span class="qa';
-$indexAfterText = strpos($page, $tagsAfterText, $indexBeforeText);
-$lengthText = $indexAfterText - $indexBeforeText;
-$msg = substr($page, $indexBeforeText, $lengthText);
+$indexBeforeText = strpos($page, $tagsBeforeText);
+$description = "";
+$title = "";
+if ($indexBeforeText) {
 
-$tagBeforeUserName = '="qa-user-link">';
-$indexStartUserName = strpos($page, $tagBeforeUserName, $indexAfterText);
-$indexStartUserName += strlen($tagBeforeUserName);
-$indexEndUserName = strpos($page, "</", $indexStartUserName); //spelling of </a> differs
-$lengthUserName = $indexEndUserName - $indexStartUserName;
-$user = substr($page, $indexStartUserName, $lengthUserName);
+    $indexBeforeText += strlen($tagsBeforeText);
+    print_debug("indexBeforeText=" . $indexBeforeText);
 
-$removedUserToken = "anonymous";
-$indexAnonymous = strpos($page, $removedUserToken, $indexAfterText);
-if ($indexAnonymous > 0 && $indexAnonymous < $indexStartUserName) {
-    $user = "[deactivated user]";
-    $indexStartUserName = $indexAnonymous;
+    $tagsAfterText = '<span class="qa';
+    $indexAfterText = strpos($page, $tagsAfterText, $indexBeforeText);
+    print_debug("indexAfterText=" . $indexAfterText);
+    $lengthText = $indexAfterText - $indexBeforeText;
+    print_debug("lengthText=" . $lengthText);
+    $msg = substr($page, $indexBeforeText, $lengthText);
+
+    $tagBeforeUserName = '="qa-user-link">';
+    $indexStartUserName = strpos($page, $tagBeforeUserName, $indexAfterText);
+    $indexStartUserName += strlen($tagBeforeUserName);
+    $indexEndUserName = strpos($page, "</", $indexStartUserName); //spelling of </a> differs
+    $lengthUserName = $indexEndUserName - $indexStartUserName;
+    $user = substr($page, $indexStartUserName, $lengthUserName);
+
+    $removedUserToken = "anonymous";
+    $indexAnonymous = strpos($page, $removedUserToken, $indexAfterText);
+    if ($indexAnonymous > 0 && $indexAnonymous < $indexStartUserName) {
+        $user = "[deactivated user]";
+        $indexStartUserName = $indexAnonymous;
+    }
+
+    $lengthBetween = $indexStartUserName - $indexAfterText;
+    $between = substr($page, $indexAfterText, $lengthBetween);
+    $what = "Answer";
+    if (stristr($between, "qa-c-item-who-data")) {
+        $what = "Comment";
+        $url = str_replace("#a", "#c", $url);
+    }
+    $description = trim(htmlentities(strip_tags($msg)));
+    $title = $what . ' by ' . $user;
+} else {
+    $title = "Answer or reply to post '" . extract_from_to($page, '<meta itemprop="name" content="', '">' . "'");
+    //$description = extract_from_to($page, '<meta name="description" content="', '">');
+    $description = "This post is too recent, please wait a few minutes to see a preview ... or just click the link and let it surprise you!";
 }
-
-$lengthBetween = $indexStartUserName - $indexAfterText;
-$between = substr($page, $indexAfterText, $lengthBetween);
-$what = "Answer";
-if (stristr($between, "qa-c-item-who-data")) {
-    $what = "Comment";
-    $url = str_replace("#a", "#c", $url);
-}
-
 header('Content-Type: text/html; charset=utf-8');
 echo "<html>";
 echo " <head>";
-echo '<meta http-equiv="refresh" content="0; URL=' . str_replace("&appId=Straub620_g2gpeek", "", $url) . '">';
-echo '  <meta property="og:description" content="' . trim(htmlentities(strip_tags($msg)))  . '">';
-echo '  <meta property="og:title" content="' . $what . ' by ' . $user .  '">';
-echo '  <title>' . $what . ' by ' . $user . ' </title>';
+if (!$is_debug) {
+    echo '<meta http-equiv="refresh" content="0; URL=' . str_replace("&appId=Straub620_g2gpeek", "", $url) . '">';
+}
+echo '  <meta property="og:description" content="' . $description  . '">';
+echo '  <meta property="og:title" content="' . $title .  '">';
+echo '  <title>' . $title . ' </title>';
 echo " </head>";
 echo " <body>";
 echo " </body>";
 echo " </html>";
+
+function print_debug($line)
+{
+    global $is_debug;
+    if ($is_debug) {
+        echo $line . "<br>\n";
+    }
+}
+
+function extract_from_to($haystack, $prefix, $suffix)
+{
+    $index_prefix = strpos($haystack, $prefix) + strlen($prefix);
+    $index_suffix = strpos($haystack, $suffix, $index_prefix);
+    $length = $index_suffix - $index_prefix;
+    // echo "$length = $index_suffix - $index_prefix<br>";
+    return substr($haystack, $index_prefix, $length);
+}
